ac5eae5aa873b126d31610a6f4bd0c364c17ad85
Md Ramzan <ramzanmd865@gmail.com>
2022-11-20 09:03:25 +0530

    Allowed Hosts

--- finalblog/settings.py
+++ finalblog/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-^g_&ngu8*-z-9kto=vaur62e#qb(u2+lztgw6g&(ihsf-k(2h=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1'
-    'blogramzan.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','blogramzan.herokuapp.com']
 
 
 # Application definition
